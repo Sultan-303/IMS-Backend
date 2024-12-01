4747105b51c482e9c022b3377dcd1746dc6e6f2d
Sultan <531213@student.fontys.nl>
2024-12-01 14:36:40 +0100

    Rest of category code completed

--- IMS.Interfaces/ICategoriesRepository.cs
+++ IMS.Interfaces/ICategoriesRepository.cs
@@ -1,9 +1,15 @@
 using IMS.DTO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace IMS.Interfaces
 {
     public interface ICategoriesRepository
     {
         Task<IEnumerable<Category>> GetAllCategoriesAsync();
+        Task<Category> GetCategoryByIdAsync(int id);
+        Task AddCategoryAsync(Category category);
+        Task UpdateCategoryAsync(Category category);
+        Task DeleteCategoryAsync(int id);
     }
 }
--- IMS.Interfaces/ICategoriesService.cs
+++ IMS.Interfaces/ICategoriesService.cs
@@ -1,10 +1,15 @@
 using IMS.DTO;
-using IMS.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace IMS.Interfaces
 {
     public interface ICategoriesService
     {
         Task<IEnumerable<Category>> GetAllCategoriesAsync();
+        Task<Category> GetCategoryByIdAsync(int id);
+        Task AddCategoryAsync(Category category);
+        Task UpdateCategoryAsync(Category category);
+        Task DeleteCategoryAsync(int id);
     }
 }
