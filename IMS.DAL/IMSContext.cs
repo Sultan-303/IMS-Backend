71b714d23998dda62f6040b6ee1ce864dcaadd90
Sultan <531213@student.fontys.nl>
2024-12-26 16:56:31 +0100

    g

--- IMS.DAL/IMSContext.cs
+++ IMS.DAL/IMSContext.cs
@@ -1,5 +1,7 @@
 using IMS.Common.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 namespace IMS.DAL
 {
@@ -12,6 +14,20 @@ namespace IMS.DAL
         public DbSet<Category> Categories { get; set; }
         public DbSet<ItemCategory> ItemCategories { get; set; }
         public DbSet<User> Users { get; set; }
+
+    private string GetConnectionString()
+    {
+        return "Host=localhost;Database=ims_db;Username=postgres;Password=postgres";
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql(GetConnectionString())
+                    .ConfigureWarnings(w => w.Ignore(RelationalEventId.PendingModelChangesWarning));
+            }
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
