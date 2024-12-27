20bf99bfa4f02b8c319f0b942aae550839b6ff96
Sultan <531213@student.fontys.nl>
2024-12-27 20:52:11 +0100

    testmigration to see if database connection is true

--- IMS.DAL/Migrations/20241227195156_AddTestColumn.Designer.cs
+++ IMS.DAL/Migrations/20241227195156_AddTestColumn.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using IMS.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace IMS.DAL.Migrations
 {
     [DbContext(typeof(IMSContext))]
-    partial class IMSContextModelSnapshot : ModelSnapshot
+    [Migration("20241227195156_AddTestColumn")]
+    partial class AddTestColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ IMS.DAL/Migrations/20241227195156_AddTestColumn.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IMS.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTestColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "PasswordHash" },
+                values: new object[] { new DateTime(2024, 12, 27, 19, 51, 55, 593, DateTimeKind.Utc).AddTicks(7638), "$2a$11$S0ZyqPPqG4Bs1vTZu0efC.0at286xGsVjmw353IQmvTtdOfbPrkx." });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "PasswordHash" },
+                values: new object[] { new DateTime(2024, 12, 27, 18, 39, 59, 581, DateTimeKind.Utc).AddTicks(6021), "$2a$11$4uqBKUpzNrJbVVBD.HWgg.B3soFqXNddxLhQHtxDgHqweEkOTDiiC" });
+        }
+    }
+}
