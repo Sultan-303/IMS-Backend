49f009368d039edaf01b599aeaf706746bbb4767
Sultan <531213@student.fontys.nl>
2024-12-27 21:04:31 +0100

    added test colum to item entity "Description"

--- IMS.Common/Entities/Item.cs
+++ IMS.Common/Entities/Item.cs
@@ -12,6 +12,7 @@ namespace IMS.Common.Entities
         public string ItemName { get; set; }
 
         public string Unit { get; set; }
+        public string Description { get; set; }
 
         public decimal Price { get; set; }
 
---
+++ IMS.DAL/Migrations/20241227200348_AddItemDescription.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IMS.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddItemDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Items",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "PasswordHash" },
+                values: new object[] { new DateTime(2024, 12, 27, 20, 3, 48, 16, DateTimeKind.Utc).AddTicks(6907), "$2a$11$WvwM2SgvVED0CUdt7a4xveDdjct3QodJjVt9KDaDw1CIRQAnGtoHC" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Items");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "PasswordHash" },
+                values: new object[] { new DateTime(2024, 12, 27, 19, 51, 55, 593, DateTimeKind.Utc).AddTicks(7638), "$2a$11$S0ZyqPPqG4Bs1vTZu0efC.0at286xGsVjmw353IQmvTtdOfbPrkx." });
+        }
+    }
+}
